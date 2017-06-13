351768c1ef22dd8f6a9bfb10d8a1550826f0002b
xe63619 <oscarjunior.gonzales.contractor@bbva.com>
2017-06-13 13:00:39 +0200

    just a try!

--- src/ch02/value-object-pattern.js
+++ src/ch02/value-object-pattern.js
@@ -66,3 +66,5 @@ const greenwich = coordinate(51.4778, 0.0015);
 console.log(greenwich.translate(10, 10).toString()); //-> '(61.4778, 10.0015)'
 console.log(greenwich.toString()); //-> '(51.4778, 0.0015)'
 console.log(princetonZip.toString()); //-> '08544-3345'
+
+console.log('Develop');
